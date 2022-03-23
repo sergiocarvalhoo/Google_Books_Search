66c3539d567c6ed23ac4a772fc9637a511ca8095
sergiocarvalhoo <sergiotutorshd@gmail.com>
2022-03-22 22:41:24 -0300

    Fix Backend API Port.

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
-const port = 4000;
+const port = process.env.PORT; 
 
 const loginRouter = require("./routes/login");
 const booksRouter = require("./routes/books");
